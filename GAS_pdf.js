357af550a0828c0a25dfc9f3401a9bd7477a52e2
fermoo025 <tbldev0411@gmail.com>
2025-06-15 05:23:18 +0300

    working

--- GAS_pdf.js
+++ GAS_pdf.js
@@ -1,7 +1,7 @@
 let dic = {},
     invalidFiles = [];
 //  1jwQ03EQ0O7Ofl8geDH21v1HXom8NNiH7
-const FOLDER_ID = '1-VBQzExkHPLyq8iAZ9d5Q8eEbBE1REy7',
+const FOLDER_ID = '1jwQ03EQ0O7Ofl8geDH21v1HXom8NNiH7',
     patPrefect = /^(北海道|青森県|岩手県|宮城県|秋田県|山形県|福島県|茨城県|栃木県|群馬県|埼玉県|千葉県|東京都|神奈川県|新潟県|富山県|石川県|福井県|山梨県|長野県|岐阜県|静岡県|愛知県|三重県|滋賀県|京都府|大阪府|兵庫県|奈良県|和歌山県|鳥取県|島根県|岡山県|広島県|山口県|徳島県|香川県|愛媛県|高知県|福岡県|佐賀県|長崎県|熊本県|大分県|宮崎県|鹿児島県|沖縄県)/;
 
 function toHalfWidth(str) {
@@ -343,9 +343,7 @@ function matchPdfUsingINumericPrice() {
 
     const folder = DriveApp.getFolderById(FOLDER_ID);
     const files = folder.getFiles();
-
-    let dic = {},
-        invalidFiles = [];
+    dic = {};invalidFiles = [];
     //let allText='';
     while (files.hasNext()) {
         const fil = files.next(),
@@ -436,27 +434,4 @@ function common() {
         }
         console.log(price + '\t' + addr + '\t' + fn);
     });
-}
-
-function matchPdfUsingINumericPrice2() {
-    const allFiles = DriveApp.getFilesByName('allText.txt');
-    const allFile = allFiles.next();
-    let allText = allFile.getBlob().getDataAsString();
-    allText = convertNFC(allText);
-    const lines = allText.split('\n');
-    let fn = '';
-    lines.forEach((line) => {
-        line = line.trim();
-        if (line == '') return;
-        if (line.endsWith('=================')) {
-            fn = line.replace('=================', '');
-            dic[fn] = {
-                text: '',
-                price: 0
-            };
-        } else {
-            dic[fn].text += line + '\n';
-        }
-    });
-    common();
 }
