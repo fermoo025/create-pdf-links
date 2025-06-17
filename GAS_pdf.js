6edeca6d079d0f49f9abe60fa3aaad81da145f07
fermoo025 <tbldev0411@gmail.com>
2025-06-17 05:24:36 +0300

    v1.0

--- GAS_pdf.js
+++ GAS_pdf.js
@@ -1,9 +1,7 @@
 let dic = {},
    invalidFiles = [];
-//  
-
-
-const FOLDER_ID = '17tCcRC5-rutB9QkvBDASJwCJbWThGiEg',
+//  1TDw4KONPyeH8Rr9EVdjfQ0GYjjnYZ2ag
+const FOLDER_ID = '1TDw4KONPyeH8Rr9EVdjfQ0GYjjnYZ2ag',
    patPrefect = /^(北海道|青森県|岩手県|宮城県|秋田県|山形県|福島県|茨城県|栃木県|群馬県|埼玉県|千葉県|東京都|神奈川県|新潟県|富山県|石川県|福井県|山梨県|長野県|岐阜県|静岡県|愛知県|三重県|滋賀県|京都府|大阪府|兵庫県|奈良県|和歌山県|鳥取県|島根県|岡山県|広島県|山口県|徳島県|香川県|愛媛県|高知県|福岡県|佐賀県|長崎県|熊本県|大分県|宮崎県|鹿児島県|沖縄県)/;
 
 
@@ -347,7 +345,25 @@ function getFileFromPrice(pr) {
    return fs;
 }
 
-
+function ex(){
+   const folder = DriveApp.getFolderById('1c6l8cZRLqtbeyyvKlp9Nm2xOJHNkaDBf');
+   const files = folder.getFiles();
+   while (files.hasNext()) {
+       const fil = files.next(), fn = fil.getName();
+        if (fn=='6555物件概要書【戸建片面】20250410.pdf'){
+          const blob = fil.getBlob();
+          const resource = {
+              title: blob.getName(),
+              mimeType: MimeType.GOOGLE_DOCS
+          };
+          const file = Drive.Files.insert(resource, blob, {
+              ocr: false
+          });
+          const doc = DocumentApp.openById(file.id), textRaw = doc.getBody().getText();
+          console.log(textRaw)
+        }
+   }
+}
 function matchPdfUsingINumericPrice() {
 
 
---
+++ callGas.py
@@ -0,0 +1,8 @@
+import requests
+
+url = 'https://script.google.com/macros/s/AKfycbyiCHn1ZYHgEn0mULf2kBA3KopNdDtJR0-28pbDWNLz4ce956w4NxnHZ4mUvmItwrUg/exec'
+data = {'folderId': '1TRXjKEfWGK8OAh5TIP8_CFjmbIHEGKul', 'sheetId': '1CVAAilMvM8LsrZIelRz6nf4SRvFeoawGonw6L4QlSC8',
+        'tabName': ''}
+
+response = requests.post(url, json=data)
+print(response.text)
